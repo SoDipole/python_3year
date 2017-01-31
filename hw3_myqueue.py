a9cf22dff71ec9375c12501d9a36c7fa2bd0bcb6
SoDipole <sarun4ik7@yandex.ru>
2017-01-31 20:29:40 +0300

    Update hw3_myqueue.py

--- hw3_myqueue.py
+++ hw3_myqueue.py
@@ -16,11 +16,9 @@ class Stack:
 class MyQueue:
     def __init__(self):
         self.stack1 = Stack()
-        self.stack2 = Stack()
-        
+        self.stack2 = Stack() 
     def enqueue(self, item): 
-        self.stack1.push(item)
-        
+        self.stack1.push(item)    
     def dequeue(self):
         for i in range(len(self.stack1.arr)):
             self.stack2.push(self.stack1.pop())
@@ -28,40 +26,25 @@ class MyQueue:
         for i in range(len(self.stack2.arr)):
             self.stack1.push(self.stack2.pop())        
         return item
-    
     def peek(self):
-        return self.stack1[0]
-    
+        return self.stack1.arr[0]
     def isEmpty(self): 
         if len(self.stack1.arr) == 0:
             return True
         else:
             return False
         
-
 items = [1,2,3,4,1,5]
-#a = Stack()
-#for item in items:
-    #a.push(item)
-    #print(a.arr)
-#print('stacked')
-#for i in range(len(items)+1):
-    #if a.isEmpty() == False:
-        #a.pop()
-        #print(a.arr)
-    #else:
-        #print('stack is empty')
-         
+     
 my = MyQueue()
-
 for item in items:
     my.enqueue(item)
     print(my.stack1.arr)
 print('queued')
+print(my.peek())
 for i in range(len(items)+1):
     if my.isEmpty() == False:
         my.dequeue()
         print(my.stack1.arr)
     else:
         print('queue is empty')
-
