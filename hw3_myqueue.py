7f4710edbfc557a09b67a955ae122a01c0602d35
SoDipole <sarun4ik7@yandex.ru>
2017-01-31 20:25:38 +0300

    Add files via upload

--- hw3_myqueue.py
+++ hw3_myqueue.py
@@ -18,49 +18,50 @@ class MyQueue:
         self.stack1 = Stack()
         self.stack2 = Stack()
         
-    def push_1(self, item): 
+    def enqueue(self, item): 
         self.stack1.push(item)
-    def push_2(self, item): 
-        self.stack2.push(item)
-    
-    def pop_1(self): 
-        return self.stack1.pop()
-    def pop_2(self): 
-        return self.stack2.pop()
-    
-    def peek_1(self): 
-        return self.stack1.peek()
-    def peek_2(self): 
-        return self.stack2.peek()
-    
-    def isEmpty_1(self): 
-        return self.stack1.isEmpty()
-    def isEmpty_2(self): 
-        return self.stack2.isEmpty()
-
-    def transfer_1to2(self, n = ''):
-        if n == '':
-            n = len(self.stack1.arr)
-        for i in range(n):
-            self.push_2(self.pop_1())
-    def transfer_2to1(self, n = ''):
-        if n == '':
-            n = len(self.stack2.arr)
-        for i in range(n):
-            self.push_1(self.pop_2())
-            
-items = [1,2,3,4,1,5]
         
-my = MyQueue()
-for i in range(len(items)):
-    my.push_1(items[i])
-    my.push_2(items[len(items)-i-1])
-    print(my.stack1.arr, my.stack2.arr)
-print('stacked')
+    def dequeue(self):
+        for i in range(len(self.stack1.arr)):
+            self.stack2.push(self.stack1.pop())
+        item = self.stack2.arr.pop()
+        for i in range(len(self.stack2.arr)):
+            self.stack1.push(self.stack2.pop())        
+        return item
+    
+    def peek(self):
+        return self.stack1[0]
+    
+    def isEmpty(self): 
+        if len(self.stack1.arr) == 0:
+            return True
+        else:
+            return False
+        
 
-my.transfer_2to1()
-print(my.stack1.arr, my.stack2.arr)
-my.transfer_1to2()
-print(my.stack1.arr, my.stack2.arr)
-my.transfer_2to1(6)
-print(my.stack1.arr, my.stack2.arr)
+items = [1,2,3,4,1,5]
+#a = Stack()
+#for item in items:
+    #a.push(item)
+    #print(a.arr)
+#print('stacked')
+#for i in range(len(items)+1):
+    #if a.isEmpty() == False:
+        #a.pop()
+        #print(a.arr)
+    #else:
+        #print('stack is empty')
+         
+my = MyQueue()
+
+for item in items:
+    my.enqueue(item)
+    print(my.stack1.arr)
+print('queued')
+for i in range(len(items)+1):
+    if my.isEmpty() == False:
+        my.dequeue()
+        print(my.stack1.arr)
+    else:
+        print('queue is empty')
+
